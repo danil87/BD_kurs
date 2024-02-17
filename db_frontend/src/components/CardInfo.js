7ceac7968998d550027b189e7cddaeccf65369bd
danil87 <kokovin.danil@list.ru>
2024-02-17 19:37:08 +0300

    create main block

---
+++ db_frontend/src/components/CardInfo.js
@@ -0,0 +1,32 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+// import './CardInfo.css'
+
+function CardInfo() {
+    return (
+        <Card className='CardInfo' sx={{ maxWidth: 345, maxHeight: 500, margin: '0 15px 30px 15px' }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://mp6.siedlce.pl/wp/wp-content/uploads/2021/03/baner2.jpg"
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    )
+}
+
+export default CardInfo;

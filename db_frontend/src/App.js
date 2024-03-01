f66786be2be2d09955bff3df251ae34f40f52070
danil87 <kokovin.danil@list.ru>
2024-03-02 00:44:54 +0300

    create reviews component

--- db_frontend/src/App.js
+++ db_frontend/src/App.js
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router-dom';
 import ClassSchedule from './components/ClassSchedule/ClassSchedule';
 import SpecialistsList from './components/SpecialiststList/SpecialistsList';
 import './App.css';
+import UsersReviews from './components/UsersReviews/UsersReviews';
+import AboutUs from './components/AboutUs/AboutUs';
 
 function App() {
   return (
@@ -12,10 +14,10 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Main />} />
-        <Route path='/schedule' element={ <ClassSchedule /> } />
+        <Route path='/schedule' element={<ClassSchedule />} />
         <Route path='/specialists' element={<SpecialistsList />} />
-        <Route path='/reviews' />
-        <Route path='/aboutUs' />
+        <Route path='/reviews' element={<UsersReviews />} />
+        {/* <Route path='/aboutUs' element={<AboutUs />} /> */}
       </Routes>
       <Footer />
     </div>
